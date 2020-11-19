82ff0d9e5298920dda51daa70a9ce6a695379401
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-11-19 18:40:05 -0300

    Se cambió el parametro de borrar a entity

--- src/main/java/ar/com/unpaz/organizerddd/domain/repositorycontracts/IRepository.java
+++ src/main/java/ar/com/unpaz/organizerddd/domain/repositorycontracts/IRepository.java
@@ -8,6 +8,6 @@ import ar.com.unpaz.organizerddd.domain.specifications.Specification;
 public interface IRepository <E>{
 	public List<E> get();
 	public void insert(E entity);
-	public void delete(int id);
+	public void delete(E entity);
 	public List<E> query(Specification<E> spec);
 }
