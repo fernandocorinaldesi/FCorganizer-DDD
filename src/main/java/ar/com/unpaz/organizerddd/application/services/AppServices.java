f7f2507f3209c775e2dce85b4f1b632974953264
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-11-19 18:31:12 -0300

    Se cambió el parametro de borrar a entity

--- src/main/java/ar/com/unpaz/organizerddd/application/services/AppServices.java
+++ src/main/java/ar/com/unpaz/organizerddd/application/services/AppServices.java
@@ -6,6 +6,6 @@ import java.util.List;
 public interface AppServices <E>{
 	public List<E> getList();
 	public boolean addEntity(E entity);
-	public void delEntity(int pswId);
+	public void delEntity(E entity);
 	public boolean existEntity(List<E> list,E entity);
 }
