043830a955395746e69d205c746d895af2f260f2
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-11-19 18:43:59 -0300

    Se implementó el repositorio en memoria para passwords

--- src/main/java/ar/com/unpaz/organizerddd/domain/repositorycontracts/InMemoryPasswordRepository.java
+++ src/main/java/ar/com/unpaz/organizerddd/domain/repositorycontracts/InMemoryPasswordRepository.java
@@ -1,9 +1,9 @@
 package ar.com.unpaz.organizerddd.domain.repositorycontracts;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 
 import ar.com.unpaz.organizerddd.domain.entitys.Password;
 
@@ -12,7 +12,6 @@ public class InMemoryPasswordRepository extends AbstractInMemoryRepository<Passw
 	private Map<String,Password> passlist;
 	private int numberOfInteractions;
 	
-	
 	public InMemoryPasswordRepository() {
 	    passlist = new HashMap<>();
 	    numberOfInteractions = 0;
@@ -20,24 +19,41 @@ public class InMemoryPasswordRepository extends AbstractInMemoryRepository<Passw
 	@Override
 	public List<Password> get() {
 		// TODO Auto-generated method stub
-		return null;
+		  List<Password> result = new ArrayList<>();
+		    for (String key : passlist.keySet()) {
+		      Password pass = passlist.get(key);
+		      int id=pass.getPassId();
+		      String site = pass.getSite();
+		      String user = pass.getUser();
+		      String psw = pass.getPass();
+		      int dni = pass.getDni();
+		      result.add(new Password(id,site,user,psw,dni));
+		    }
+		    return result;
 	}
 
 	@Override
-	public void delete(int id) {
+	public void delete(Password psw) {
 		// TODO Auto-generated method stub
-		
+		 passlist.remove(String.valueOf(psw.getPassId()));
 	}
 
 	@Override
 	public void insert(Password entity) {
 		// TODO Auto-generated method stub
 		numberOfInteractions = getNumberOfInteractions() + 1;
-	    passlist.put(entity.getSite(),entity);
+		passlist.put(String.valueOf(numberOfInteractions),adapter(entity));
 		
 	}
 	public int getNumberOfInteractions() {
 		return numberOfInteractions;
+	}
+	private Password adapter(Password entity) {
+		return new Password(getNumberOfInteractions(),
+				            entity.getSite(),
+				            entity.getUser(),
+				            entity.getPass(),
+				            entity.getDni());
 	}
 
 }
