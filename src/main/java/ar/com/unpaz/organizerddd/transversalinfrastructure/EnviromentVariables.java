5582c46f0455f65d3efaff3e826f6fe271c0646d
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-11-29 16:17:14 -0300

    Se creo una clase  para guardar variables usadas por todas las capas

---
+++ src/main/java/ar/com/unpaz/organizerddd/transversalinfrastructure/EnviromentVariables.java
@@ -0,0 +1,8 @@
+package ar.com.unpaz.organizerddd.transversalinfrastructure;
+
+public class EnviromentVariables {
+	
+	 public static String APPERRORS="";
+	 public static boolean INMEMORY=false;
+
+}
