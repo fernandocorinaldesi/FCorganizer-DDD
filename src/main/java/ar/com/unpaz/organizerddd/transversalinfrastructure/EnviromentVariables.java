4f3b0cf2e58dfe9ae4a7c5e361867d99cf4a28fe
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-11-30 13:25:21 -0300

    Se agregaron las variables de user y login del admin

--- src/main/java/ar/com/unpaz/organizerddd/transversalinfrastructure/EnviromentVariables.java
+++ src/main/java/ar/com/unpaz/organizerddd/transversalinfrastructure/EnviromentVariables.java
@@ -4,5 +4,7 @@ public class EnviromentVariables {
 	
 	 public static String APPERRORS="";
 	 public static boolean INMEMORY=false;
+	 public static String ADMINUSER="admin";
+	 public static String ADMINPASS="admin";
 
 }
