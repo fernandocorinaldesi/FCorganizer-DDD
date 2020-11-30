e27fd9d71af4b18b505cf688abc614a72f005dfa
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-11-30 13:18:49 -0300

    Se removio el hardcoded de user

--- src/main/java/ar/com/unpaz/organizerddd/infrastructure/DbConection.java
+++ src/main/java/ar/com/unpaz/organizerddd/infrastructure/DbConection.java
@@ -34,7 +34,8 @@ public class DbConection {
 		FileInputStream fis = new FileInputStream(path);
 		prop.loadFromXML(fis);
 		final String url = prop.getProperty("url");
-		return DriverManager.getConnection(url,"sa","");
+		final String user = prop.getProperty("user");
+		return DriverManager.getConnection(url,user,"");
 	}
 
 }
