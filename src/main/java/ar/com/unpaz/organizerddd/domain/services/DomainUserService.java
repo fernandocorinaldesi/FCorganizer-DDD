4a5f632d0dec2d5370a9bc53ac506585a6c0d70f
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-11-30 13:16:35 -0300

    Se agregaron validaciones a los servicios del dominio

--- src/main/java/ar/com/unpaz/organizerddd/domain/services/DomainUserService.java
+++ src/main/java/ar/com/unpaz/organizerddd/domain/services/DomainUserService.java
@@ -5,9 +5,24 @@ import ar.com.unpaz.organizerddd.domain.entitys.User;
 public class DomainUserService implements IDomainServices<User>{
 
 	@Override
-	public boolean validate(User entity) {
+	public boolean validate(User user) {
 		// TODO Auto-generated method stub
-		return false;
+		return user.emptyPassVal() ||
+				   user.emptyUserVal() ||
+				   user.emptyNameVal() ||
+				   user.emptyIntDniVal() ||
+				   user.emptysecondNameVal() ||
+				   user.MinIntDniVal() ||
+				   user.MinPassVal() ||
+				   user.MinNameVal() ||
+				   user.MinUserVal() ||
+				   user.MinsecondNameVal() ||
+				   user.MaxIntDniVal() ||
+				   user.MaxsecondNameVal()||
+				   user.MaxPassVal() ||
+				   user.MaxNameVal() ||
+				   user.MaxUserVal()
+				   ;
 	}
 
 }
