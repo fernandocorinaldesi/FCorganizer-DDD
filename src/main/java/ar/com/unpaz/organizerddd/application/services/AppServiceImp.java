9bf5a19f29170afac8d7221d90c589623ded1025
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-11-30 13:18:09 -0300

    Se agregaron validaciones al update del servicio de aplicación

--- src/main/java/ar/com/unpaz/organizerddd/application/services/AppServiceImp.java
+++ src/main/java/ar/com/unpaz/organizerddd/application/services/AppServiceImp.java
@@ -6,15 +6,14 @@ import ar.com.unpaz.organizerddd.domain.repositorycontracts.IRepository;
 import ar.com.unpaz.organizerddd.domain.services.IDomainServices;
 import ar.com.unpaz.organizerddd.transversalinfrastructure.EnviromentVariables;
 
+public abstract class AppServiceImp<E> implements AppServices<E> {
 
-public abstract class AppServiceImp<E> implements AppServices<E>{
-	
 	protected IRepository<E> repository;
 	private IDomainServices<E> domainservices;
-		
-	public AppServiceImp(IRepository<E> repository, IDomainServices<E> domainservices){
-		this.repository=repository;
-		this.domainservices=domainservices;
+
+	public AppServiceImp(IRepository<E> repository, IDomainServices<E> domainservices) {
+		this.repository = repository;
+		this.domainservices = domainservices;
 	}
 
 	@Override
@@ -26,27 +25,32 @@ public abstract class AppServiceImp<E> implements AppServices<E>{
 	@Override
 	public boolean addEntity(E pass) {
 		// TODO Auto-generated method stub
-		if(existEntity(getList(),pass)) {
-        EnviromentVariables.APPERRORS="el registro ya existe en la base de datos";
-		return false;
+		if (existEntity(getList(), pass)) {
+			EnviromentVariables.APPERRORS = "el registro ya existe en la base de datos";
+			return false;
+		} else if (domainservices.validate(pass)) {
+			EnviromentVariables.APPERRORS = "campos invalidos";
+			return false;
+		} else {
+			repository.insert(pass);
+			return true;
 		}
-		else if(domainservices.validate(pass)){
-		EnviromentVariables.APPERRORS="campos invalidos";
-		return false;
-		}
-		else
-		repository.insert(pass);
-		return true;	
 
 	}
+
 	@Override
 	public boolean updateEntity(E entity) {
 		// TODO Auto-generated method stub
-		repository.update(entity);
-		return true;	
+		if (domainservices.validate(entity)) {
+			EnviromentVariables.APPERRORS = "campos invalidos";
+			return false;
 
+		} else {
+			repository.update(entity);
+			return true;
+		}
 	}
-	
+
 	@Override
 	public List<E> getList() {
 		// TODO Auto-generated method stub
@@ -54,19 +58,13 @@ public abstract class AppServiceImp<E> implements AppServices<E>{
 	}
 
 	@Override
-	public boolean existEntity(List<E> list,E entity) {
+	public boolean existEntity(List<E> list, E entity) {
 		// TODO Auto-generated method stub
-		for(E e : list) {
-			if(e.equals(entity))
+		for (E e : list) {
+			if (e.equals(entity))
 				return true;
 		}
 		return false;
 	}
 
 }
-
-
-
-	
-
-
