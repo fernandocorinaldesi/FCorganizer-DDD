7cd8bb5330b5b4f3e9707694887a082bc0475681
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-12-08 23:49:06 -0300

    Se quito el metodo ya innecesario de setear el usuario logeado

--- src/main/java/ar/com/unpaz/organizerddd/application/services/AppServicePass.java
+++ src/main/java/ar/com/unpaz/organizerddd/application/services/AppServicePass.java
@@ -3,14 +3,14 @@ package ar.com.unpaz.organizerddd.application.services;
 import java.util.List;
 
 import ar.com.unpaz.organizerddd.domain.entitys.Password;
-import ar.com.unpaz.organizerddd.domain.entitys.User;
 import ar.com.unpaz.organizerddd.domain.repositorycontracts.IRepository;
 import ar.com.unpaz.organizerddd.domain.services.IDomainServices;
 import ar.com.unpaz.organizerddd.domain.specifications.ByDniSpecification;
 import ar.com.unpaz.organizerddd.domain.specifications.Specification;
+import ar.com.unpaz.organizerddd.transversalinfrastructure.EnviromentVariables;
 
 public class AppServicePass extends AppServiceImp<Password>{
-	   private User user;
+
 		public AppServicePass(IRepository<Password> passwordrepository, 
 				             IDomainServices<Password> domainservices) {
 			
@@ -20,13 +20,11 @@ public class AppServicePass extends AppServiceImp<Password>{
 			
 			// TODO Auto-generated constructor stub
 		}
-		public void setUser(User user) {
-			this.user=user;
-		}
+	
 		@Override
 		public List<Password> getList() {
 			// TODO Auto-generated method stub
-			Specification<Password>spec=new  ByDniSpecification(this.user.getDni());
+			Specification<Password>spec=new  ByDniSpecification(EnviromentVariables.loggedUserDni);
 			return this.repository.query(spec);
 		};
 
